d4d28dc747256ead0d2dbaaea57e15ea3722a1fa
L-pixela <122270958+L-pixela@users.noreply.github.com>
2025-02-25 21:05:06 +0700

    BLA-001- START CODE

---
+++ flutter_2_course/lib/service/rides_service.dart
@@ -0,0 +1,35 @@
+import 'package:week_3_blabla_project/model/ride_pref/ride_pref.dart';
+
+import '../dummy_data/dummy_data.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+
+  ///
+  ///  Return the relevant rides, given the passenger preferences
+  ///
+  static List<Ride> getRidesFor(RidePref preferences) {
+    // print(availableRides);
+
+    // For now, just a test
+    return availableRides
+        .where((ride) =>
+            ride.departureLocation == preferences.departure &&
+            ride.arrivalLocation == preferences.arrival)
+        .toList();
+  }
+}
+
+void main() {
+  // to see the amount of availables ride
+  print("Ride Availables: ${RidesService.availableRides.length}");
+  // Test the service
+  RidesService.availableRides.forEach((rides) {
+    print("Ride From ${rides.arrivalLocation} To ${rides.arrivalLocation}");
+  });
+}
