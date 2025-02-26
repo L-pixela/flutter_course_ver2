90667e03f21847198c593655c67a0842e16d2c57
L-pixela <122270958+L-pixela@users.noreply.github.com>
2025-02-26 23:54:56 +0700

    BLA-004 – Implement Ride Preferences Form

--- flutter_2_course/lib/screens/ride_pref/widgets/ride_pref_form.dart
+++ flutter_2_course/lib/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
- 
+import 'package:week_3_blabla_project/theme/theme.dart';
+import 'package:week_3_blabla_project/utils/date_time_util.dart';
+import 'package:week_3_blabla_project/widgets/actions/bla_button.dart';
+import 'package:week_3_blabla_project/widgets/display/bla_divider.dart';
+
 import '../../../model/ride/locations.dart';
 import '../../../model/ride_pref/ride_pref.dart';
- 
+
 ///
 /// A Ride Preference From is a view to select:
-///   - A depcarture location
+///   - A departure location
 ///   - An arrival location
 ///   - A date
 ///   - A number of seats
@@ -28,8 +32,6 @@ class _RidePrefFormState extends State<RidePrefForm> {
   Location? arrival;
   late int requestedSeats;
 
-
-
   // ----------------------------------
   // Initialize the Form attributes
   // ----------------------------------
@@ -37,29 +39,98 @@ class _RidePrefFormState extends State<RidePrefForm> {
   @override
   void initState() {
     super.initState();
-    // TODO 
+    departureDate = DateTime.now();
+    requestedSeats = 1;
+
+    // Initialize if there's an existing RidePref
+    if (widget.initRidePref != null) {
+      departure = widget.initRidePref!.departure;
+      arrival = widget.initRidePref!.arrival;
+      departureDate = widget.initRidePref!.departureDate;
+      requestedSeats = widget.initRidePref!.requestedSeats;
+    }
   }
 
   // ----------------------------------
   // Handle events
   // ----------------------------------
- 
+
+  //Switch Departure and Arrival Locations
+  void _switchLocations() {
+    if (departure != null && arrival != null) {
+      setState(() {
+        final temp = departure;
+        departure = arrival;
+        arrival = temp;
+      });
+    }
+  }
 
   // ----------------------------------
   // Compute the widgets rendering
   // ----------------------------------
-  
+
+  //Input Field Tile
+  Widget inputFieldTile(IconData leadIcon, String label, IconData? trailIcon,
+      VoidCallback onTapped) {
+    return ListTile(
+      leading: Icon(leadIcon),
+      title: Text(
+        label,
+        style: BlaTextStyles.body,
+      ),
+      trailing: trailIcon != null
+          ? IconButton(
+              onPressed: _switchLocations,
+              icon: Icon(trailIcon),
+              color: BlaColors.primary,
+            )
+          : null,
+      onTap: onTapped,
+    );
+  }
 
   // ----------------------------------
   // Build the widgets
   // ----------------------------------
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [ 
- 
-        ]);
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      decoration: BoxDecoration(
+          color: BlaColors.white, borderRadius: BorderRadius.circular(18)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          //Depature Field
+          inputFieldTile(
+              Icons.radio_button_off,
+              departure?.name ?? "Choose your Location",
+              Icons.swap_vert,
+              () {}),
+          BlaDivider(),
+          //Arrival Field
+          inputFieldTile(
+              Icons.radio_button_off,
+              arrival != null
+                  ? "${arrival!.name}, ${arrival!.country}"
+                  : "Choose your Destination",
+              null,
+              () {}),
+          BlaDivider(),
+          //DepatureDate Field
+          inputFieldTile(Icons.calendar_month,
+              DateTimeUtils.formatDateTime(departureDate), null, () {}),
+          BlaDivider(),
+          //Requested Seats Field
+          inputFieldTile(Icons.people, "$requestedSeats", null, () {}),
+          //Booking Button
+          BlaButton(
+              type: BlaButtonType.primary,
+              label: "Search",
+              onPressedBlaButton: () {}),
+        ],
+      ),
+    );
   }
 }
